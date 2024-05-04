8b5e5ff5dacec1b60bc256621323838d147fb0bb
jonathansan04 <tatansantos4@gmail.com>
2024-05-04 11:42:33 -0500

    Front funcionalidades

--- cliente/src/Screens/home.js
+++ cliente/src/Screens/home.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import './estilosscreens.css'
 
 const Home = ({ sesionid }) => {
     let navigate = useNavigate();
@@ -6,11 +7,106 @@ const Home = ({ sesionid }) => {
         localStorage.removeItem('Sessionid');
         navigate('/app/inicio');
     }
+
+    
     return (
         <div style={{background: "#fff8"}}>
             <h1>
-                Bienvenido
+                Bienvenidooo
             </h1>
+            
+            <section className='tarjetas'>
+
+            <div className='secciontarjeta'>
+            <h3 className='titulotarjeta'>
+                Finca
+            </h3>
+            <button className='botontarjeta' >
+                   Crear finca
+             </button>
+             <button className='boton2'>
+                   Consultar finca
+             </button>
+            </div>
+            </section>
+ 
+
+            <section className='tarjetaspot'>
+
+            <div className='secciontarjeta'>
+            <h3 className='titulotarjeta'>
+                Potreros
+            </h3>
+            <button className='botontarjeta'  >
+                   Crear potrero
+             </button>
+             <button className='boton2'>
+                   Consultar potrero
+             </button>
+            </div>
+            </section>
+
+            <section className='tarjetasani'>
+
+            <div className='secciontarjeta'>
+            <h3 className='titulotarjeta'>
+                Animales
+            </h3>
+            <button className='botontarjeta' >
+                   Crear animal
+             </button>
+             <button className='boton2'>
+                   Consultar animal
+             </button>
+            </div>
+            </section>
+
+            <section className='tarjetas'>
+
+            <div className='secciontarjeta'>
+            <h3 className='titulotarjeta'>
+                Vacunacion
+            </h3>
+            <button className='botontarjeta'>
+                   Crear vacunación
+             </button>
+             <button className='boton2'>
+                   Consultar vacunación
+             </button>
+            </div>
+            </section>
+
+            <section className='tarjetasvent'>
+
+            <div className='secciontarjeta'>
+            <h3 className='titulotarjeta'>
+                Ventas
+            </h3>
+            <button className='botontarjeta'>
+                   Crear venta
+             </button>
+             <button className='boton2'>
+                   Consultar venta
+             </button>
+            </div>
+            </section>
+
+            <section className='tarjetasemp'>
+
+            <div className='secciontarjeta'>
+            <h3 className='titulotarjeta'>
+                Empleado
+            </h3>
+            <button className='botontarjeta'>
+                   Crear empleado
+             </button>
+             <button className='boton2'>
+                   Consultar empleado
+             </button>
+            </div>
+            </section>
+           
+
             <h3>
                 Información de Inicio
             </h3>
